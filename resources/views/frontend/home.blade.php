cab138bd7ed0e1c3e51e80b652dd2accb840e769
jovanovic-nemanja <69627607+jovanovic-nemanja@users.noreply.github.com>
2020-10-20 04:06:07 +0900

    medication
    medication

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -36,8 +36,7 @@
                                     </div><!-- end row -->
                                     <div class="col-xs-12 mb-30">
                                         <div class="form-group no-mb">
-                                            <a href="#" class="btn btn-primary btn-lg gradient-blue" style="width:100%; margin-bottom: 2%;"> Medications</a>
-                                            <!-- <a href="{{ route('usermedications.indexusermedication', $resident->id) }}" class="btn btn-primary btn-lg gradient-blue" style="width:100%; margin-bottom: 2%;"> Medications</a> -->
+                                            <a href="{{ route('usermedications.indexusermedication', $resident->id) }}" class="btn btn-primary btn-lg gradient-blue" style="width:100%; margin-bottom: 2%;"> Medications</a>
 
                                             <div class="ui-dropdowns" data-example-id="single-button-dropdown">
                                                 <div class="btn-group" style="width: 100%;">
@@ -45,6 +44,7 @@
                                                         Daily Activities <span class="caret"></span>
                                                     </button>
                                                     <ul class="dropdown-menu" role="menu">
+                                                        <li><a href="{{ route('useractivities.indexuseractivity', $resident->id) }}">View ADL</a></li>
                                                         <li><a href="{{ route('useractivities.createuseractivity', ['type' => 1, 'resident' => $resident->id]) }}">Primary ADL</a></li>
                                                         <li><a href="{{ route('useractivities.createuseractivity', ['type' => 2, 'resident' => $resident->id]) }}">Secondary ADL</a></li>
                                                     </ul>
