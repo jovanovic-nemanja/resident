eee74ccd4edb5c2da784050085d37d210d560dec
jovanovic-nemanja <69627607+jovanovic-nemanja@users.noreply.github.com>
2020-11-01 17:34:49 +0900

    reminder medication

--- app/Http/Controllers/Admin/NotificationsController.php
+++ app/Http/Controllers/Admin/NotificationsController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 use App\User;
+use Carbon\Carbon;
+use App\Medications;
 use App\Notifications;
+use App\Assignmedications;
 
 class NotificationsController extends Controller
 {
@@ -23,6 +27,88 @@ class NotificationsController extends Controller
 
     public function getNotificationdata(Request $request)
     {
+        $cur_date = User::getformattime();
+        $assign_medications = DB::table('assign_medications')
+                            ->select('assign_medications.*', 'medications.*', 'assign_medications.sign_date as assign_date', 'users.*', 'medications.name as med_name', 'users.name as u_name')
+                            ->Join('medications', 'medications.id', '=', 'assign_medications.medications')
+                            ->Join('users', 'users.id', '=', 'assign_medications.resident')
+                            ->get();
+
+        if (@$assign_medications) {
+            foreach ($assign_medications as $assign_medication) {
+                $ass_date = Carbon::parse($assign_medication->assign_date);
+                $cur_date['dates'] = Carbon::parse($cur_date['date']); 
+
+                if ($ass_date->addDays($assign_medication->duration) >= $cur_date['dates']) {   
+                    $assign_time1 = $assign_medication->time1;
+                    if ($assign_time1) {
+                        $startTime = Carbon::parse(User::formattime1($assign_time1));
+                        $finishTime = Carbon::parse(User::formattime1($cur_date['time']));
+                        if ($startTime > $finishTime) {
+                            $sym = "";
+                        }else{
+                            $sym = "-";
+                        }
+                        $totalDuration1 = $sym.$finishTime->diffInSeconds($startTime);
+                    }else {
+                        $totalDuration1 = "";
+                    }
+
+                    $assign_time2 = $assign_medication->time2;
+                    if ($assign_time2) {
+                        $startTime = Carbon::parse(User::formattime1($assign_time2));
+                        $finishTime = Carbon::parse(User::formattime1($cur_date['time']));
+                        if ($startTime > $finishTime) {
+                            $sym = "";
+                        }else{
+                            $sym = "-";
+                        }
+                        $totalDuration2 = $sym.$finishTime->diffInSeconds($startTime);
+                    }else {
+                        $totalDuration2 = "";
+                    }
+
+                    $assign_time3 = $assign_medication->time3;
+                    if ($assign_time3) {
+                        $startTime = Carbon::parse(User::formattime1($assign_time3));
+                        $finishTime = Carbon::parse(User::formattime1($cur_date['time']));
+                        if ($startTime > $finishTime) {
+                            $sym = "";
+                        }else{
+                            $sym = "-";
+                        }
+                        $totalDuration3 = $sym.$finishTime->diffInSeconds($startTime);
+                    }else {
+                        $totalDuration3 = "";
+                    }
+
+                    $assign_time4 = $assign_medication->time4;
+                    if ($assign_time4) {
+                        $startTime = Carbon::parse(User::formattime1($assign_time4));
+                        $finishTime = Carbon::parse(User::formattime1($cur_date['time']));
+                        if ($startTime > $finishTime) {
+                            $sym = "";
+                        }else{
+                            $sym = "-";
+                        }
+                        $totalDuration4 = $sym.$finishTime->diffInSeconds($startTime);
+                    }else {
+                        $totalDuration4 = "";
+                    }
+
+                    if ($totalDuration1 == 300 || $totalDuration2 == 300 || $totalDuration3 == 300 || $totalDuration4 == 300) {
+                        $record = Notifications::create([
+                            'user_name' => 'admin',
+                            'resident_name' => $assign_medication->u_name,
+                            'contents' => $assign_medication->med_name,
+                            'is_read' => 1,
+                            'sign_date' => $cur_date['date'],
+                        ]);
+                    }
+                }
+            }
+        }
+        
         $results = Notifications::where('is_read', 1)->get();
 
         return response()->json($results);
@@ -30,6 +116,10 @@ class NotificationsController extends Controller
 
     public function updateIsread(Request $request)
     {
+        $dates = User::getformattime();
+        $date = $dates['date'];
+        $time = $dates['time'];
+
         if (@$request->notificationId) {
             $record = Notifications::where('id', $request->notificationId)->first();
             $record->is_read = 2;
