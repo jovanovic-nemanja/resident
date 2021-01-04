e25c7e684c33232a92d0c7dfb230f42e2ec5c58c
jovanovic-nemanja <69627607+jovanovic-nemanja@users.noreply.github.com>
2021-01-05 01:02:54 +0900

    login page

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -8,7 +8,7 @@
         <div class="d-flex flex-column-auto flex-column pt-lg-40 pt-15">
             <!--begin::Aside header-->
             <a href="{{ route('home') }}" class="text-center mb-10">
-                <img src="{{ asset('finaldesign/assets/media/logos/logo-letter-1.png') }}" class="max-h-70px" alt="" />
+                <img src="{{ asset('images/logo.png') }}" class="max-h-70px" alt="" />
             </a>
             <!--end::Aside header-->
             <!--begin::Aside title-->
@@ -18,7 +18,7 @@
         </div>
         <!--end::Aside Top-->
         <!--begin::Aside Bottom-->
-        <div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-y-bottom bgi-position-x-center" style="background-image: url({{ asset('finaldesign/assets/media/svg/illustrations/login-visual-1.svg') }})"></div>
+        <div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-y-bottom bgi-position-x-center" style="background-image: url({{ asset('images/login_bg.jpeg') }})"></div>
         <!--end::Aside Bottom-->
     </div>
     <!--begin::Aside-->
